87b812d2f8621ed32fea3b205b2fd242a0fff201
MakhshafSharif <108417493+MakhshafSharif@users.noreply.github.com>
2022-07-25 16:11:27 +0500

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        function runAjax() {
+            var inputVal = document.getElementById("Hello").value;
+            if (inputVal == "") return;
+            $.ajax({
+                url: 'WebForm1.aspx/DisplayData',
+                type: 'post',
+                data: JSON.stringify({ "Carname": inputVal }),
+                contentType: 'application/json',
+                async: false,
+                success: function (data) {
+                    var OUTPUT = document.getElementById("OUTPUT");
+                    OUTPUT.innerHTML = data.d;
+                }
+            })
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Enter some text here:</h1>
+            <input type="text" runat="server" id="Hello"  onkeyup="runAjax()"/>
+           <p runat="server" id="OUTPUT"></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [System.Web.Services.WebMethod]
+        public static List<string> DisplayData(string Carname)
+        {
+            var dispCars = new List<string>();//temperory array in which we will store our data..
+            var matchList = carList();
+            foreach (string x in matchList)
+            {
+                if (x.StartsWith(Carname)) // string1.StartsWith("Welcome");
+                {
+                    dispCars.Add(x);
+                }
+            }
+            return dispCars;
+        }
+        public class Cars
+        {
+            public string name { get; set; }
+
+        }
+        public static List<string> carList()
+        {
+            var myList = new List<string>();
+            myList.Add("Chevrolet" );
+            myList.Add("Honda");
+            myList.Add("Nissan");
+            myList.Add("Ford");
+            myList.Add("Fiat");
+            myList.Add("Jeep");
+            myList.Add("Volkswagen");
+            myList.Add("Volvo");
+            myList.Add("Jaguar");
+            myList.Add("Audi");
+            myList.Add("Toyota");
+            myList.Add("Civic");
+            myList.Add("Lexus");
+            myList.Add("Porshce");
+            myList.Add("Bugatti");
+            myList.Add("Bentley");
+
+            return myList;
+        }
+    }
+}
